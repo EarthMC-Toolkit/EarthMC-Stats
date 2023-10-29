fea36ec4bbf05081468b18d1a4b67ee1d17c8acf
O.S <owen77stubbs@gmail.com>
2023-10-29 04:00:44 +0000

    route command

--- aurora/slashcommands/route.ts
+++ aurora/slashcommands/route.ts
@@ -1,13 +1,50 @@
-import type { Client, ChatInputCommandInteraction } from "discord.js"
+import { 
+    type Client, 
+    type ChatInputCommandInteraction, 
+    type SlashCommandIntegerOption,
+    EmbedBuilder, 
+    SlashCommandBuilder
+} from "discord.js"
+
 import { Aurora } from "earthmc"
+import { embedField } from "../../bot/utils/fn.js"
+
+const desc = "Returns info on how to get to a location quickly or safely."
+
+const CoordIntOption = (
+    option: SlashCommandIntegerOption, coord: string, 
+    min?: number, max?: number
+) => {
+    option.setName(coord)
+    option.setDescription(`The ${coord.toUpperCase()} coordinate.`)
+    option.setRequired(true)
+        
+    if (min) option.setMinValue(min)
+    if (max) option.setMaxValue(max)
+
+    return option
+}
 
 export default {
     name: "route",
-    description: "",
-    run: async (client: Client, interaction: ChatInputCommandInteraction) => {
-        const x = interaction.options.getString("x")
-        const z = interaction.options.getString("z")
+    description: desc,
+    run: async (_: Client, interaction: ChatInputCommandInteraction) => {
+        const x = interaction.options.getInteger("x")
+        const z = interaction.options.getInteger("z")
         
-        const gps = Aurora.GPS.findRoute()
-    }
+        const route = await Aurora.GPS.fastestRoute({ x, z })
+        const embed = new EmbedBuilder()
+            .setColor("Random")
+            .setFields(
+                embedField("Nearest Nation", `/n spawn ${route.nation.name}`,  true),
+                embedField("Distance", `${route.distance} Blocks`, true),
+                embedField("Direction", route.direction, true),
+            )
+
+        return interaction.reply({ embeds: [embed] })
+    }, data: new SlashCommandBuilder()
+        .setName("route")
+        .setDescription(desc)
+        .addIntegerOption(opt => CoordIntOption(opt, "x", -33280, 33081))
+        .addIntegerOption(opt => CoordIntOption(opt, "z", -16640, 16508))
 }
