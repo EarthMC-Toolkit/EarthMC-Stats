e3b3ccd7d3655f3797d14e4034982b9ac900475f
O3H <owen77stubbs@gmail.com>
2024-04-13 21:21:52 +0100

    .

--- aurora/buttons/export.ts
+++ aurora/buttons/export.ts
@@ -8,7 +8,7 @@ import { Button } from "../../bot/types.js"
 const exportBtn: Button = {
     id: "export",
     execute: (_: Client, interaction: ChatInputCommandInteraction) => {
-        return interaction.reply({ 
+        return interaction.reply({
             content: "Your exported content is attached below.",
             ephemeral: true,
             files: []
